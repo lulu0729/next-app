df9354b857781e5c77d944f030e71c20c54450bf
lulu0729 <34208317+lulu0729@users.noreply.github.com>
2023-11-08 14:57:01 +0000

    Deploying to gh-pages from @ lulu0729/next-app@29d2f78479a3021c311ab1ae16499cb364513bac 🚀

---
+++ _next/static/wk2lmlpiMJwdzdWs5YoRl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-69fd9f4707fa171a.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
